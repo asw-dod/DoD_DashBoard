e115ad7291cbcb56624b82a3efa7df72e3d6742f
INMD1 <lyw514549@gmail.com>
2022-03-24 15:03:43 +0900

    기초 디자인 끝

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app class="mmain">
+    <v-app-bar app white>
+      <v-toolbar-title>ASW D.o.D Dap Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>[maybe weather]</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <div class="ma-5">
+        <v-row>
+          <v-col>
+            <h2>학사공지</h2>
+            <v-card class="card-size">
+              <div v-for="itme in 10" :key="itme">
+                <v-card-title class="font-weight-bold"
+                  >학사공지입니다.</v-card-title
+                >
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col>
+            <h2>취업</h2>
+            <v-card class="card-size">
+              <div v-for="itme in 10" :key="itme">
+                <v-card-title class="font-weight-bold"
+                  >취업입니다.</v-card-title
+                >
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col>
+            <h2>ASW공지</h2>
+            <v-card class="card-size">
+              <div v-for="itme in 10" :key="itme">
+                <v-card-title class="font-weight-bold"
+                  >ASW공지입니다.</v-card-title
+                >
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col>
+            <h2>학과공지</h2>
+            <v-card class="card-size">
+              <div v-for="itme in 10" :key="itme">
+                <v-card-title class="font-weight-bold"
+                  >응소공지입니다.</v-card-title
+                >
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <h2>비교과</h2>
+            <v-card class="card-size">
+              <div v-for="itme in 10" :key="itme">
+                <v-card-title class="font-weight-bold"
+                  >비교과입니다.</v-card-title
+                >
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col>
+            <h2>취업공지</h2>
+            <v-card class="card-size">
+              <div v-for="itme in 10" :key="itme">
+                <v-card-title class="font-weight-bold"
+                  >취업공지입니다.</v-card-title
+                >
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col>
+            <h2>몰?루공지</h2>
+            <v-card class="card-size">
+              <div v-for="itme in 10" :key="itme">
+                <v-card-title class="font-weight-bold"
+                  >몰?루공지입니다.</v-card-title
+                >
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col>
+            <h2>식사공지</h2>
+            <v-card class="card-size">
+              <div v-for="itme in 10" :key="itme">
+                <v-card-title class="font-weight-bold"
+                  >식사공지입니다.</v-card-title
+                >
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data: () => ({}),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
