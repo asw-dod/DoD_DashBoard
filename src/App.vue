11ddbea6b8afd0759fed403dc251c92bfd19170f
INMD1 <87979171+INMD1@users.noreply.github.com>
2022-03-25 17:25:08 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <v-app-bar app white>
       <v-toolbar-title>ASW D.o.D Dap Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-toolbar-title>Test 중입니다.</v-toolbar-title>
+      <v-spacer></v-spacer>
       <v-toolbar-title>[maybe weather]</v-toolbar-title>
     </v-app-bar>
     <v-main>
@@ -12,9 +14,8 @@
             <h2>학사공지</h2>
             <v-card class="card-size">
               <div v-for="itme in 10" :key="itme">
-                <v-card-title class="font-weight-bold"
-                  >학사공지입니다.</v-card-title
-                >
+                <v-card-title v-animate-css="'flipInX'" class="font-weight-bold">
+                  학사공지입니다.</v-card-title>
                 <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
               </div>
             </v-card>
@@ -89,18 +90,62 @@
           </v-col>
           <v-col>
             <h2>식사공지</h2>
-            <v-card class="card-size">
-              <div v-for="itme in 10" :key="itme">
-                <v-card-title class="font-weight-bold"
-                  >식사공지입니다.</v-card-title
-                >
-                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
-              </div>
-            </v-card>
+            <v-carousel
+              cycle
+              height="400"
+              :show-arrows="false"
+              hide-delimiter-background
+              delimiter-icon="mdi-minus"
+            >
+              <v-carousel-item>
+                <h1>행복기숙사</h1>
+                <h2>아침</h2>
+                <p class="memu-size">
+                  된장찌개 방어무조림 감자채피망볶음 가지나물 김치
+                </p>
+                <h2>점심</h2>
+                <p class="memu-size">
+                  [한식]오징어국 돈두루치기 부추전 숙주나물 김치 <br> 
+                  [일품]오징어국 돈두루치기 부추전 숙주나물 김치
+                </p>
+                <h2>저녁</h2>
+                <p class="memu-size">
+                  부대찌개 새우까스&크리미소스 매콤떡볶이 호박나물 깍두기
+                </p>
+              </v-carousel-item>
+              <v-carousel-item>
+                <h1>효민기숙사</h1>
+                <v-col>
+                  <h2>아침</h2>
+                  <p class="memu-size">
+                    된장찌개 방어무조림 감자채피망볶음 가지나물 김치
+                  </p>
+                  <h2>점심</h2>
+                  <p class="memu-size">
+                    오징어국 돈두루치기 부추전 숙주나물 김치
+                  </p>
+                  <h2>저녁</h2>
+                  <p class="memu-size">
+                    부대찌개 새우까스&크리미소스 매콤떡볶이 호박나물 깍두기
+                  </p>
+                </v-col>
+              </v-carousel-item>
+              <v-carousel-item>
+                <h1>정보관</h1>
+              </v-carousel-item>
+              <v-carousel-item>
+                <h1>수덕전</h1>
+              </v-carousel-item>
+            </v-carousel>
           </v-col>
         </v-row>
       </div>
     </v-main>
+    <v-footer app>
+      Made by INMD
+      <v-spacer></v-spacer>
+      Update 2022-03.25
+    </v-footer>
   </v-app>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-
+ 
 import './css/style.css'
+import VAnimateCss from 'v-animate-css';
+
+
 Vue.config.productionTip = false
+
+Vue.use(VAnimateCss);
 
 new Vue({
   vuetify,
