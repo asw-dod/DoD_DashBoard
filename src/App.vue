71a9f37eb1aa8f99f65e2af7924fbacd3c55c8dc
INMD1 <lyw514549@gmail.com>
2022-03-27 01:47:52 +0900

    1차 프로토타입 제작 완료!

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@
     <v-app-bar app white>
       <v-toolbar-title>ASW D.o.D Dap Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-title>Test 중입니다.</v-toolbar-title>
-      <v-spacer></v-spacer>
       <v-toolbar-title>[maybe weather]</v-toolbar-title>
     </v-app-bar>
     <v-main>
@@ -13,43 +11,61 @@
           <v-col>
             <h2>학사공지</h2>
             <v-card class="card-size">
-              <div v-for="itme in 10" :key="itme">
-                <v-card-title v-animate-css="'flipInX'" class="font-weight-bold">
-                  학사공지입니다.</v-card-title>
-                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+              <div v-for="itme in Bachelor" :key="itme">
+                <v-card-title
+                  v-animate-css="'flipInX'"
+                  class="font-weight-bold"
+                >
+                  {{ itme.title }}</v-card-title
+                >
+                <v-card-subtitle v-animate-css="'flipInX'" class="pb-0">
+                  {{ itme.date }}
+                </v-card-subtitle>
               </div>
             </v-card>
           </v-col>
           <v-col>
             <h2>취업</h2>
             <v-card class="card-size">
-              <div v-for="itme in 10" :key="itme">
-                <v-card-title class="font-weight-bold"
-                  >취업입니다.</v-card-title
+              <div v-for="itme in internship" :key="itme">
+                <v-card-title
+                  v-animate-css="'flipInX'"
+                  class="font-weight-bold"
                 >
-                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+                  {{ itme[0] + itme[1] }}</v-card-title
+                >
               </div>
             </v-card>
           </v-col>
           <v-col>
             <h2>ASW공지</h2>
             <v-card class="card-size">
-              <div v-for="itme in 10" :key="itme">
-                <v-card-title class="font-weight-bold"
-                  >ASW공지입니다.</v-card-title
+              <div v-for="itme in Entrepreneurship" :key="itme">
+                <v-card-title
+                  v-animate-css="'flipInX'"
+                  class="font-weight-bold"
                 >
-                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+                  {{ itme.title }}</v-card-title
+                >
+                <v-card-subtitle v-animate-css="'flipInX'" class="pb-0">
+                  {{ itme.date }}
+                </v-card-subtitle>
               </div>
             </v-card>
           </v-col>
           <v-col>
             <h2>학과공지</h2>
             <v-card class="card-size">
-              <div v-for="itme in 10" :key="itme">
-                <v-card-title class="font-weight-bold"
-                  >응소공지입니다.</v-card-title
+              <div v-for="itme in academic" :key="itme">
+                <v-card-title
+                  v-animate-css="'flipInX'"
+                  class="font-weight-bold"
                 >
-                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+                  {{ itme.title }}</v-card-title
+                >
+                <v-card-subtitle v-animate-css="'flipInX'" class="pb-0">
+                  {{ itme.date }}
+                </v-card-subtitle>
               </div>
             </v-card>
           </v-col>
@@ -58,38 +74,53 @@
           <v-col>
             <h2>비교과</h2>
             <v-card class="card-size">
-              <div v-for="itme in 10" :key="itme">
-                <v-card-title class="font-weight-bold"
-                  >비교과입니다.</v-card-title
+              <div v-for="itme in non_discipline" :key="itme">
+                <v-card-title
+                  v-animate-css="'flipInX'"
+                  class="font-weight-bold"
                 >
-                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+                  {{ itme.title }}</v-card-title
+                >
+                <v-card-subtitle v-animate-css="'flipInX'" class="pb-0">
+                  {{ itme.date }}
+                </v-card-subtitle>
               </div>
             </v-card>
           </v-col>
           <v-col>
             <h2>취업공지</h2>
             <v-card class="card-size">
-              <div v-for="itme in 10" :key="itme">
-                <v-card-title class="font-weight-bold"
-                  >취업공지입니다.</v-card-title
+              <div v-for="itme in job_announcement" :key="itme">
+                <v-card-title
+                  v-animate-css="'flipInX'"
+                  class="font-weight-bold"
                 >
-                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+                  {{ itme.title }}</v-card-title
+                >
+                <v-card-subtitle v-animate-css="'flipInX'" class="pb-0">
+                  {{ itme.date }}
+                </v-card-subtitle>
               </div>
             </v-card>
           </v-col>
           <v-col>
-            <h2>몰?루공지</h2>
+            <h2>학교공지</h2>
             <v-card class="card-size">
-              <div v-for="itme in 10" :key="itme">
-                <v-card-title class="font-weight-bold"
-                  >몰?루공지입니다.</v-card-title
+              <div v-for="itme in school" :key="itme">
+                <v-card-title
+                  v-animate-css="'flipInX'"
+                  class="font-weight-bold"
                 >
-                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+                  {{ itme.title }}</v-card-title
+                >
+                <v-card-subtitle v-animate-css="'flipInX'" class="pb-0">
+                  {{ itme.date }}
+                </v-card-subtitle>
               </div>
             </v-card>
           </v-col>
           <v-col>
-            <h2>식사공지</h2>
+            <h2>오늘 기숙사 식단공지</h2>
             <v-carousel
               cycle
               height="400"
@@ -98,43 +129,42 @@
               delimiter-icon="mdi-minus"
             >
               <v-carousel-item>
-                <h1>행복기숙사</h1>
+                <!-- <v-sheet height="100%" color="blue lighten-5"> -->
+                <h2>행복기숙사</h2>
                 <h2>아침</h2>
                 <p class="memu-size">
-                  된장찌개 방어무조림 감자채피망볶음 가지나물 김치
+                  {{ this.happy["아침"] }}
                 </p>
                 <h2>점심</h2>
                 <p class="memu-size">
-                  [한식]오징어국 돈두루치기 부추전 숙주나물 김치 <br> 
-                  [일품]오징어국 돈두루치기 부추전 숙주나물 김치
+                  [한식]{{ this.happy["점심"] }}<br />[일품]{{
+                    this.happy["점심특"]
+                  }}
                 </p>
                 <h2>저녁</h2>
                 <p class="memu-size">
-                  부대찌개 새우까스&크리미소스 매콤떡볶이 호박나물 깍두기
+                  [한식]{{ this.happy["저녁"] }}<br />[일품]{{
+                    this.happy["저녁특"]
+                  }}
                 </p>
+                <!-- </v-sheet> -->
               </v-carousel-item>
               <v-carousel-item>
-                <h1>효민기숙사</h1>
+                <h2>효민기숙사</h2>
                 <v-col>
                   <h2>아침</h2>
                   <p class="memu-size">
-                    된장찌개 방어무조림 감자채피망볶음 가지나물 김치
+                    {{ this.hyomin["아침"] }}
                   </p>
                   <h2>점심</h2>
                   <p class="memu-size">
-                    오징어국 돈두루치기 부추전 숙주나물 김치
+                    {{ this.hyomin["점심"] }}
                   </p>
                   <h2>저녁</h2>
                   <p class="memu-size">
-                    부대찌개 새우까스&크리미소스 매콤떡볶이 호박나물 깍두기
+                    {{ this.hyomin["저녁"] }}
                   </p>
                 </v-col>
-              </v-carousel-item>
-              <v-carousel-item>
-                <h1>정보관</h1>
-              </v-carousel-item>
-              <v-carousel-item>
-                <h1>수덕전</h1>
               </v-carousel-item>
             </v-carousel>
           </v-col>
@@ -144,15 +174,142 @@
     <v-footer app>
       Made by INMD
       <v-spacer></v-spacer>
-      Update 2022-03.25
+      Update: {{ this.Uptime }}
     </v-footer>
+    n>
   </v-app>
 </template>
 
 <script>
+import dayjs from "dayjs";
+import axios from "axios";
+
 export default {
   name: "App",
   components: {},
-  data: () => ({}),
+  data: function () {
+    return {
+      Uptime: "",
+      hyomin: "",
+      happy: "",
+      job_announcement: [],
+      Bachelor: [],
+      non_discipline: [],
+      internship: [],
+      Entrepreneurship: [],
+      academic: [],
+      school: [],
+    };
+  },
+  method: {},
+  async mounted() {
+    //변수를 적는 곳
+    const happy_url =
+      "https://dorm.deu.ac.kr/deu/food/getWeeklyMenu.kmc?locgbn=DE&sch_date=";
+    const hyomin_url =
+      "https://dorm.deu.ac.kr/hyomin/food/getWeeklyMenu.kmc?locgbn=DE&sch_date=";
+
+    //fun을 적는곳(반복을 위해서 fund을 섰다.)
+    //이 fun은 기숙사를 파싱해서 저장하는 함수이다.
+    async function getfood(url) {
+      const nowdate = dayjs().format("YYYY-MM-DD");
+      const responce = await axios(url + nowdate);
+      if (url.indexOf("hyomin") != -1) {
+        const data = responce.data["root"][0].WEEKLYMENU[0];
+        for (let index = 0; index < 8; index++) {
+          if (data["fo_date" + index] == nowdate) {
+            return {
+              아침: data["fo_menu_mor" + index],
+              점심: data["fo_menu_lun" + index],
+              저녁: data["fo_menu_eve" + index],
+            };
+          }
+        }
+      } else {
+        for (let main_index = 0; main_index < 2; main_index++) {
+          const data = responce.data["root"][0].WEEKLYMENU[main_index];
+          for (
+            let index = 4 * main_index;
+            index < 4 + 4 * main_index;
+            index++
+          ) {
+            if (data["fo_date" + index] == nowdate) {
+              return {
+                아침: data["fo_menu_mor" + index],
+                점심: data["fo_menu_lun" + index],
+                점심특: data["fo_sub_lun" + index],
+                저녁: data["fo_menu_eve" + index],
+                저녁특: data["fo_sub_eve" + index],
+              };
+            }
+          }
+        }
+      }
+    }
+
+    //처음에 한번만 이벤트가 발생하는곳
+    this.Uptime = dayjs();
+    this.happy = await getfood(happy_url);
+    this.hyomin = await getfood(hyomin_url);
+
+    const response = await axios.get(
+      "https://api.github.com/repos/asw-dod/dap-macro/issues"
+    );
+
+    if (response.data[0].title.indexOf("DAP") != -1) {
+      const json = JSON.parse(response.data[0].body);
+      this.Bachelor = json["학사공지"].notice;
+      this.job_announcement = json["취업공지"].notice;
+      this.non_discipline = json["비교과"].notice;
+      this.internship = json["인턴십"].notice;
+      const json1 = JSON.parse(response.data[1].body);
+      this.Entrepreneurship = json1["창업교육센터"];
+      this.academic = json1["학과공지"];
+      this.school = json1["학교공지"];
+    } else {
+      const json = JSON.parse(response.data[1].body);
+      this.Bachelor = json["학사공지"].notice;
+      this.job_announcement = json["취업공지"].notice;
+      this.non_discipline = json["비교과"].notice;
+      this.internship = json["인턴십"].notice;
+      const json1 = JSON.parse(response.data[0].body);
+      this.Entrepreneurship = json1["창업교육센터"];
+      this.academic = json1["학과공지"];
+      this.school = json1["학교공지"];
+    }
+
+    //몇시간 마다 반복하는 이벤트가 발생하는 곳
+    setInterval(async () => {
+      this.Uptime = dayjs();
+      this.happy = await getfood(happy_url);
+      this.hyomin = await getfood(hyomin_url);
+
+      const response = await axios.get(
+        "https://api.github.com/repos/asw-dod/dap-macro/issues"
+      );
+
+      if (response.data[0].title.indexOf("DAP") != -1) {
+        const json = JSON.parse(response.data[0].body);
+        this.Bachelor = json["학사공지"].notice;
+        this.job_announcement = json["취업공지"].notice;
+        this.non_discipline = json["비교과"].notice;
+        this.internship = json["인턴십"].notice;
+        const json1 = JSON.parse(response.data[1].body);
+        this.Entrepreneurship = json1["창업교육센터"];
+        this.academic = json1["학과공지"];
+        this.school = json1["학교공지"];
+      } else {
+        const json = JSON.parse(response.data[1].body);
+        this.Bachelor = json["학사공지"].notice;
+        this.job_announcement = json["취업공지"].notice;
+        this.non_discipline = json["비교과"].notice;
+        this.internship = json["인턴십"].notice;
+        const json1 = JSON.parse(response.data[0].body);
+        this.Entrepreneurship = json1["창업교육센터"];
+        this.academic = json1["학과공지"];
+        this.school = json1["학교공지"];
+      }
+    }, 60000);
+  },
 };
 </script>
